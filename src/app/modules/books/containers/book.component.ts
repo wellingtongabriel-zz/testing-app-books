af7cee936bc8d8100cb8b5cd851a99d7d2ec44b3
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-06-17 17:00:54 -0300

    feature/criacao-layout-login - refatoração code review

---
+++ src/app/modules/books/containers/book.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ioa-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
+})
+export class BookComponent {}
